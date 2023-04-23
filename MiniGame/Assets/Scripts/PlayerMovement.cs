8509a36c4d22bffbbb870ea483d4c581daf8dba1
Minho Kim <music4537@naver.com>
2023-04-24 00:04:28 +0900

    feat : Flipping added

--- MiniGame/Assets/Scripts/PlayerMovement.cs
+++ MiniGame/Assets/Scripts/PlayerMovement.cs
@@ -13,7 +13,14 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update(){
+        float horizontalInput = Input.GetAxis("Horizontal");
         body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        // Flip player when moving left-right
+        if(horizontalInput < -0.01f)
+            transform.localScale = Vector3.one;
+        else if(horizontalInput > 0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
 
         if(Input.GetKey(KeyCode.Space))
         body.velocity = new Vector2(body.velocity.x, speed);
