1974fa07f4a434e31f6dfa668a7cb578d1e530fd
Minho Kim <music4537@naver.com>
2023-04-23 23:52:14 +0900

    feat : Added Jumping function

--- MiniGame/Assets/Scripts/PlayerMovement.cs
+++ MiniGame/Assets/Scripts/PlayerMovement.cs
@@ -13,6 +13,9 @@ public class PlayerMovement : MonoBehaviour
     }
 
     private void Update(){
-        body.velocity = new Vector2(Input.GetAxis("Horizontal"),body.velocity.y);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        if(Input.GetKey(KeyCode.Space))
+        body.velocity = new Vector2(body.velocity.x, speed);
     }
 }
